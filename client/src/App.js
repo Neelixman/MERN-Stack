cb709af12cdd6b9124baa6cc98245c2d5d26a471
Neelix <deekwstaz@gmail.com>
2020-12-04 22:44:14 +0200

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useEffect } from "react";
+import "./App.css";
+// Components
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
+
+import Alert from "./components/layout/Alert";
+
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
